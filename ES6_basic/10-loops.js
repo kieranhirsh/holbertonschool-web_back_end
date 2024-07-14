cc48ad4de23e3d2f797a5803d9fac82b8cc818d4
kieranhirsh <kieranhirsh@gmail.com>
2024-07-14 23:09:49 +1000

    ES6 Basics. Task 10. For...of Loops. linter

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,6 +1,6 @@
 export default function appendToEachArrayValue(array, appendString) {
   let idx = 0;
-  let arr = [];
+  const arr = [];
 
   for (const value of array) {
     arr[idx] = appendString + value;
