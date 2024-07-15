f8399f4fb0e16fb55224071b93a26f8fbdae8660
kieranhirsh <kieranhirsh@gmail.com>
2024-07-16 02:06:23 +1000

    fixing typos

--- ES6_classes/8-hbtn_class.js
+++ ES6_classes/8-hbtn_class.js
@@ -1,7 +1,7 @@
 export default class HolbertonClass {
   constructor(size, location) {
-    this.size = size;
-    this.location = location;
+    this._size = size;
+    this._location = location;
   }
 
   get size() {
