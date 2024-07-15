2f38fbc580aba6dbfc4a1532c416d6cc3b4558ec
kieranhirsh <kieranhirsh@gmail.com>
2024-07-16 01:21:19 +1000

    ES6 classes. Task 6. Inheritance

---
+++ ES6_classes/6-sky_high.js
@@ -0,0 +1,27 @@
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this.floors = floors;
+  }
+
+  get sqft() {
+    return super.sqft;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  set floors(newFl) {
+    if ((typeof newFl) !== 'number') {
+      throw new TypeError('Floors must be a number');
+    }
+    this._floors= newFl;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors`
+  }
+}
