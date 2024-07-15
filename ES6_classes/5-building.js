a4de213ff1e669194cc89e08cc45ddbdf7c8e923
kieranhirsh <kieranhirsh@gmail.com>
2024-07-16 01:17:31 +1000

    ES6 classes. Task 5. A Building. linter

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -9,7 +9,7 @@ export default class Building {
       throw new Error('Class extending Building must override evacuationWarningMessage');
     }
   }
-  
+
   get sqft() {
     return this._sqft;
   }
