2d0a3136e682cb6d11f6aa83447165928de31000
kieranhirsh <kieranhirsh@gmail.com>
2024-07-16 01:13:52 +1000

    ES6 classes. Task 5. A Building. fixing typos

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -3,8 +3,8 @@ export default class Building {
     this.sqft = sqft;
 
     if (
-      this.constructor === Building
-      && typeof this.evacuationWarningMessage !== 'function'
+      (this.constructor === Building)
+      && (typeof this.evacuationWarningMessage !== 'function')
     ) {
       throw Error('Class extending Building must override evacuationWarningMessage');
     }
