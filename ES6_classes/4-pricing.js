52a036790df64d9f03e6062e2ab158b9d74f13a4
kieranhirsh <kieranhirsh@gmail.com>
2024-07-16 00:58:31 +1000

    ES6 classes. Task 4. Pricing

---
+++ ES6_classes/4-pricing.js
@@ -0,0 +1,38 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(newAm) {
+    if ((typeof newAm) !== 'number') {
+      throw new TypeError('Amount must be a number');
+    }
+    this._amount = newAm;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(newCurrency) {
+    if (!(currency instanceof Currency)) {
+      throw new TypeError('Currency must be a curency instance');
+    }
+    this._currency = newCurrency;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.displayFullCurrency()}`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
