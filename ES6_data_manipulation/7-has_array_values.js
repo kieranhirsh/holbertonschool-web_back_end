cc9c35329e043e9c69ecbb7b1632511502f13a55
kieranhirsh <kieranhirsh@gmail.com>
2024-07-17 21:39:55 +1000

    ES6 data manipulation. Task 7. More set data structure. new approach

--- ES6_data_manipulation/7-has_array_values.js
+++ ES6_data_manipulation/7-has_array_values.js
@@ -1,5 +1,4 @@
 export default function hasValuesFromArray(set, array) {
-  const setArray = new Set(array);
-  const setSet = new Set(set);
-  return setSet.isSupersetOf(setArray);
+  const result = array.map((i) => set.has(i));
+  return (!result.includes(false));
 }
