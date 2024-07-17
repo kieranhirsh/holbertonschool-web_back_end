a496bbb09cf8de43e1d3967fb2b6cfea940fec13
kieranhirsh <kieranhirsh@gmail.com>
2024-07-17 20:07:13 +1000

    ES6 data manipulation. Task 4. Combine. linter

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -2,12 +2,13 @@ export default function updateStudentGradeByCity(students, city, newGrades) {
   const cityStudents = students.filter((x) => x.location === city);
   const gradedStudents = cityStudents.map((student) => {
     const userGrade = newGrades.filter((grade) => grade.studentId === student.id);
+    const newStudent = student;
     if (userGrade.length > 0) {
-      student.grade = userGrade[0].grade;
+      newStudent.grade = userGrade[0].grade;
     } else {
-      student.grade = 'N/A';
+      newStudent.grade = 'N/A';
     }
-    return student;
+    return newStudent;
   });
 
   return gradedStudents;
