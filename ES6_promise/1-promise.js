111c647ff4391baa5542a0d8a01ee109b4db4875
kieranhirsh <kieranhirsh@gmail.com>
2024-07-21 21:55:02 +1000

    ES6 Promises. Task 1. Don't make a promise...if you know you can't keep it

---
+++ ES6_promise/1-promise.js
@@ -0,0 +1,14 @@
+export default function getFullResponseFromAPI(success) {
+  const myPromise = new Promise((resolve, reject) => {
+    if (success === true) {
+      resolve({
+        status: 200,
+        body: 'Success',
+      });
+    } else {
+      reject(Error('The fake API is not working currently'));
+    }
+  });
+
+  return myPromise;
+}
