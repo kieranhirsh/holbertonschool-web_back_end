8d493879842cda08b48231b8ba9c79f9b60a6471
kieranhirsh <kieranhirsh@gmail.com>
2024-07-21 22:19:38 +1000

    ES6 Promises. Task 6. Handle multiple promises. linter

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,7 +1,7 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default async function handleProfileSignup(firstName, lastName, fileName) {
+export default function handleProfileSignup(firstName, lastName, fileName) {
   const user = signUpUser(firstName, lastName)
     .then((value) => ({
       status: 'fulfilled',
@@ -14,5 +14,5 @@ export default async function handleProfileSignup(firstName, lastName, fileName)
       value: e.toString(),
     }));
 
-  return await Promise.all([user, photo]);
+  return Promise.all([user, photo]);
 }
