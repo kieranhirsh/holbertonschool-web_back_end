d6bcf023bd1982a6a11e2eb6141d8d331d278728
kieranhirsh <kieranhirsh@gmail.com>
2024-08-24 17:48:08 +1000

    Python - Variable Annotations. Task 9. Let's duck type an iterable object

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """Task 9"""
-from typing import Iterable, Sequence, Tuple
+from typing import Iterable, List, Sequence, Tuple
 
 
-def element_length(lst: Iterable[Sequence]) -> Tuple[Iterable[Sequence], int]:
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence], int]:
     """Task 9"""
     return [(i, len(i)) for i in lst]
