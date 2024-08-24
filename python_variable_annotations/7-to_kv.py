911b3dcd68429dd8f308a97909be061b72901dcb
kieranhirsh <kieranhirsh@gmail.com>
2024-08-24 17:25:30 +1000

    Python - Variable Annotations. Task 7. Complex types - string and int/float to tuple. reading the question explains the question

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 """Task 7"""
-import numpy as np
 from typing import Union
 
 
 def to_kv(k: str, v: Union[int, float]) -> tuple[str, float]:
     """Task 7"""
-    return (k, np.sqrt(v))
+    return (k, v * v)
