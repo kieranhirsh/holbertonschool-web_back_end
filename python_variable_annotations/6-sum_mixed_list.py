83c94d47f8d87132690ebfff5fc58c4f2bc8c51c
kieranhirsh <kieranhirsh@gmail.com>
2024-08-24 17:08:57 +1000

    Python - Variable Annotations. Task 6. Complex types - mixed list

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Task 5"""
+from typing import List, Union
+
+
+def sum_mixed_list(input_list: List[Union[float, int]]) -> float:
+    """Task 5"""
+    return sum(input_list)
