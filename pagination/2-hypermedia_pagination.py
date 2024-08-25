6d27beff515ef06ce1408b5fad1fabdaf8fc3f81
kieranhirsh <kieranhirsh@gmail.com>
2024-08-25 21:03:53 +1000

    Pagination. Task 2. Hypermedia pagination

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Task 1"""
 import csv
+import math
 from typing import List
 index_range = __import__('0-simple_helper_function').index_range
 
@@ -36,3 +37,31 @@ class Server:
             return []
         else:
             return data[start:end]
+
+    def get_hyper(self, page: int = 1, page_size: int = 10):
+        """Task 2"""
+        assert (isinstance(page, int) and page > 0)
+        assert (isinstance(page_size, int) and page > 0)
+
+        data = self.get_page(page, page_size)
+
+        total_pages = math.ceil(len(self.dataset()) / page_size)
+
+        if page == 1:
+            prev_page = None
+        else:
+            prev_page = page - 1
+
+        if page == total_pages:
+            next_page = None
+        else:
+            next_page = page + 1
+
+        return {
+            'page_size': len(data),
+            'page': page,
+            'data': data,
+            'next_page': next_page,
+            'prev_page': prev_page,
+            'total_pages': total_pages
+        }
