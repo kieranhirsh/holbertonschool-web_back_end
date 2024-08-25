6a9e171c12eecc7ec232718ee6761bba4db83c4d
kieranhirsh <kieranhirsh@gmail.com>
2024-08-25 19:31:24 +1000

    Python - Async Comprehension. Task 1. Async Comprehensions. code style validation

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -4,6 +4,7 @@ from typing import List
 
 async_generator = __import__('0-async_generator').async_generator
 
+
 async def async_comprehension() -> List[float]:
     """Task 1"""
     return [i async for i in async_generator()]
