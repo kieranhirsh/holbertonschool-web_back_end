5143310043b4af5ff763b252d683e8b853e14339
kieranhirsh <kieranhirsh@gmail.com>
2024-08-28 22:06:27 +1000

    NoSQL. Task 12. Log stats

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -4,7 +4,7 @@ from pymongo import MongoClient
 
 if __name__ == "__main__":
     """task 12"""
-    client = MongoClient("mongodb://localhost:27017/")
+    client = MongoClient('mongodb://127.0.0.1:27017')
     collection = client.logs.nginx
 
     methods = {
